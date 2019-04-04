0d77c1cc16545770a9a909dcde618a08b4cb2862
TrimurthuluPosimsetti <40907951+TrimurthuluPosimsetti@users.noreply.github.com>
2019-04-04 18:57:16 +0530

    Update LLMiddleNode.java

--- LLMiddleNode.java
+++ LLMiddleNode.java
@@ -1,48 +1,41 @@
+/*FINDING MIDDLE NODE IN THE SINGLE LINKEDLIST*/
 import java.util.*;
 import java.util.Scanner;
 
-class Node{
-  int data;
-  Node next=null;
-  Node(int data){
-    this.data=data;
-  }
+/*Here is the structre of the node*/
+class Node {
+    int data;
+    Node next = null;
+    Node(int data) {
+        this.data = data;
+    }
 }
-class LLMiddleNode {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    Node head=null;
-    Node temp=null;
-    for(int i=0;i<n;i++){
-      if(i==0){
-        Node x=new Node(sc.nextInt());
-        head=x;
-        temp=head;
-      }
-      else{
-        Node x=new Node(sc.nextInt());
-        temp.next=x;
-        temp=temp.next;
-      }
+
+/*Main Class*/
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //reading size of the linkedlist
+        Node head = null; //creating an head node
+        Node temp = null; //creating an temp node
+        for (int i = 0; i < n; i++) { //iterating for n times
+            if (i == 0) {
+                Node x = new Node(sc.nextInt()); //new head node creation
+                head = x;
+                temp = head; //assigning head to temp
+            } else {
+                Node x = new Node(sc.nextInt()); //new node creation
+                temp.next = x; //linking temp pointer to new node
+                temp = temp.next; //incrementing temp
+            }
+        }
+        //checking whether length of LinkedList is even or not
+        int c = 0;//temporary variable to breaking condition
+        temp = head;
+        while (c != (n / 2)) { //finding the middle Node
+            temp = temp.next;//incrementing temp to next node
+            c = c + 1;
+        }
+        System.out.println(temp.data);
     }
-    if(n%2==0){
-      int c=0;
-      temp=head;
-      while(c!=(n/2)){
-        temp=temp.next;
-        c=c+1;
-      }
-      System.out.println(temp.data);
-    }
-    else{
-      int c=0;
-      temp=head;
-      while(c!=n/2){
-          temp=temp.next;
-          c=c+1;
-      }
-      System.out.println(temp.data);
-    }
-  }
 }
